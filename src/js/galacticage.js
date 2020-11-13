b879e0fc5fc8e04abbcddaad848d95080fea2080
Jerrod Styrk <jstyrk@citadel.edu>
2020-11-13 10:09:09 -0800

    pass venusAge test

--- src/js/galacticage.js
+++ src/js/galacticage.js
@@ -2,10 +2,16 @@ export class GalacticAge {
   constructor(earthAge) {
     this.earthAge = earthAge;
     this.mercuryAge = 0;
+    this.venusAge = 0;
   }
 
   calcMercury(earthAge) {
     let age = this.earthAge / (6/25);
     return this.mercuryAge = parseFloat(age.toPrecision(4));
+  }
+
+  calcVenus(earthAge) {
+    let age = this.earthAge / (31/50);
+    return this.venusAge = parseFloat(age.toPrecision(4));
   }
 };
