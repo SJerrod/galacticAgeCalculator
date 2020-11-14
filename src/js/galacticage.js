029a8685be39957600c2f9d34568041c2ffbd728
Jerrod Styrk <jstyrk@citadel.edu>
2020-11-13 17:02:18 -0800

    add index and styling

--- src/js/galacticage.js
+++ src/js/galacticage.js
@@ -1,7 +1,7 @@
 export class GalacticAge {
-  constructor(earthAge, sex, continent) {
+  constructor(earthAge, gender, continent) {
     this.earthAge = earthAge;
-    this.sex = sex;
+    this.gender = gender;
     this.continent = continent;
     this.mercuryAge = 0;
     this.venusAge = 0;
@@ -11,92 +11,90 @@ export class GalacticAge {
     this.extra = [];
   }
 
-  calcMercury(earthAge) {
+  calcMercury() {
     let age = this.earthAge / .24;
     return this.mercuryAge = parseFloat(age.toPrecision(4));
   }
 
-  calcVenus(earthAge) {
+  calcVenus() {
     let age = this.earthAge / .62;
     return this.venusAge = parseFloat(age.toPrecision(4));
   }
 
-  calcMars(earthAge) {
+  calcMars() {
     let age = this.earthAge / 1.88;
     return this.marsAge = parseFloat(age.toPrecision(4));
   }
 
-  calcJupiter(earthAge) {
+  calcJupiter() {
     let age = this.earthAge / 11.86;
     return this.jupiterAge = parseFloat(age.toPrecision(4));
   }
 
-  lifeExpectancy(earthAge, sex, continent) {
+  lifeExpectancy() {
     let lifeExpect = 0;
-    switch (this.sex) {
-      case ("Male"):
-        switch (this.continent) {
-          case ("Africa"):
-            lifeExpect += 62;
-            break;
-          case ("Asia"):
-            lifeExpect += 76;
-            break;
-          case ("Australia"):
-            lifeExpect += 82;
-            break;
-          case ("Europe"):
-            lifeExpect += 79;
-            break;
-          case ("North-America"):
-            lifeExpect += 77;
-            break;
-          case ("South-America"):
-            lifeExpect += 72;
-          break;
-        }
+    switch (this.gender) {
+    case ("Male"):
+      switch (this.continent) {
+      case ("Africa"):
+        lifeExpect += 62;
         break;
-      case ("Female"):
-        switch (this.continent) {
-          case ("Africa"):
-            lifeExpect += 65;
-            break;
-          case ("Asia"):
-            lifeExpect += 80;
-            break;
-          case ("Australia"):
-            lifeExpect += 85;
-            break;
-          case ("Europe"):
-            lifeExpect += 84;
-            break;
-          case ("North-America"):
-            lifeExpect += 81;
-            break;
-          case ("South-America"):
-            lifeExpect += 78;
-          break;
-        }
+      case ("Asia"):
+        lifeExpect += 76;
         break;
+      case ("Australia"):
+        lifeExpect += 82;
+        break;
+      case ("Europe"):
+        lifeExpect += 79;
+        break;
+      case ("North-America"):
+        lifeExpect += 77;
+        break;
+      case ("South-America"):
+        lifeExpect += 72;
+        break;
+      }
+      break;
+    case ("Female"):
+      switch (this.continent) {
+      case ("Africa"):
+        lifeExpect += 65;
+        break;
+      case ("Asia"):
+        lifeExpect += 80;
+        break;
+      case ("Australia"):
+        lifeExpect += 85;
+        break;
+      case ("Europe"):
+        lifeExpect += 84;
+        break;
+      case ("North-America"):
+        lifeExpect += 81;
+        break;
+      case ("South-America"):
+        lifeExpect += 78;
+        break;
+      }
+      break;
     }
     if (this.earthAge <= lifeExpect) {
       let timeLeft= lifeExpect - this.earthAge;
-      let earthRemain = parseFloat((timeLeft).toPrecision(4))
-      let mercuryRemain = parseFloat((timeLeft / .24).toPrecision(4))
-      let venusRemain = parseFloat((timeLeft / .62).toPrecision(4))
-      let marsRemain = parseFloat((timeLeft / 1.88).toPrecision(4))
-      let jupiterRemain = parseFloat((timeLeft / 11.86).toPrecision(4))
-      this.remain.push(earthRemain, mercuryRemain, venusRemain, marsRemain, jupiterRemain)
-      return this.remain;
+      let earthRemain = parseFloat((timeLeft).toPrecision(4));
+      let mercuryRemain = parseFloat((timeLeft / .24).toPrecision(4));
+      let venusRemain = parseFloat((timeLeft / .62).toPrecision(4));
+      let marsRemain = parseFloat((timeLeft / 1.88).toPrecision(4));
+      let jupiterRemain = parseFloat((timeLeft / 11.86).toPrecision(4));
+      this.remain.push(earthRemain, mercuryRemain, venusRemain, marsRemain, jupiterRemain);
     } else {
       let timeLeft = this.earthAge - lifeExpect;
-      let earthExtra = parseFloat((timeLeft).toPrecision(4))
-      let mercuryExtra = parseFloat((timeLeft / .24).toPrecision(4))
-      let venusExtra = parseFloat((timeLeft / .62).toPrecision(4))
-      let marsExtra = parseFloat((timeLeft / 1.88).toPrecision(4))
-      let jupiterExtra = parseFloat((timeLeft / 11.86).toPrecision(4))
-      this.extra.push(earthExtra, mercuryExtra, venusExtra, marsExtra, jupiterExtra)
-      return this.extra;
+      let earthExtra = parseFloat((timeLeft).toPrecision(4));
+      let mercuryExtra = parseFloat((timeLeft / .24).toPrecision(4));
+      let venusExtra = parseFloat((timeLeft / .62).toPrecision(4));
+      let marsExtra = parseFloat((timeLeft / 1.88).toPrecision(4));
+      let jupiterExtra = parseFloat((timeLeft / 11.86).toPrecision(4));
+      this.extra.push(earthExtra, mercuryExtra, venusExtra, marsExtra, jupiterExtra);
     }
   }
-};
+}
